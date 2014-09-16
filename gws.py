d3eabe79c665212ce18ac001b30a28edd6728b11
Claudio Pisa <clauz@ninux.org>
2014-09-16 17:09:52 +0200

    find only the set of the originators of the default

--- gws.py
+++ gws.py
@@ -10,27 +10,20 @@ tree = etree.parse('gwlist.xml')
 
 #for e in tree.xpath('/pdml/packet/proto[@name="olsr"]/field[@name="olsr.message"]/field[@name="olsr.message_type" and @value="04"]'):
 
-class OlsrMessage:
-    pass
-
-msglist = []
+originatorset = set()
 
 for f in tree.xpath('/pdml/packet/proto[@name="olsr"]/field[@name="olsr.message"]'):
-    m = OlsrMessage()
+    o = ""
     for e in f:
         propname = e.get("name").replace(".","_")
-        setattr(m, propname, e.get("show"))
-    msglist.append(m)
-        
-hnas = [m for m in msglist if m.olsr_message_type == "4"]
+        propvalue = e.get("show")
+        if propname == "olsr_message_type" and propvalue != "4":
+            break
+        if propname == "olsr_origin_addr":
+            o = propvalue
+        elif propname == "olsr_network_addr":
+            if propvalue == "0.0.0.0":
+                originatorset.add(o)
 
-gws = [h for h in hnas if h.olsr_network_addr == "0.0.0.0"]
-
-#for g in gws:
-#    print g.olsr_origin_addr, g.olsr_network_addr, g.olsr_netmask
-
-gworiginators = list(set([g.olsr_origin_addr for g in gws]))
-for o in gworiginators:
-    print o
-
+print originatorset
 
